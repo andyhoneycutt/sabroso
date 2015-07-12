faa7701f8d53c10edd77bedf5d3cb698a86e6dcb
andyhoneycutt <ahoneycutt@gmail.com>
2015-07-11 19:34:31 -0600

    Added MongoDB support
    router now pulls projects from database

--- python/api/app.py
+++ python/api/app.py
@@ -1,35 +1,53 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, abort, make_response
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-projects = [
-    {
-        '_id' : 1,
-        'name' : 'Project 1',
-        'created_on' : '2015-07-11 12:00:00',
-        'modified_on' : '2015-07-11 12:00:00'
-    },
-    {
-        '_id' : 2,
-        'name' : 'Project 2',
-        'created_on' : '2015-07-11 13:00:00',
-        'modified_on' : '2015-07-11 13:30:00'
-    },
-]
+""" Try to get a connection to MongoDB, or fail out """
+try:
+    db = MongoClient('localhost')
+except pymongo.errors.ConnectionFailure:
+    exit("Could not connect to MongoDB is it running?")
 
+""" Get a list of projects """
 @app.route('/api/v1.0/projects', methods=['GET'])
 def get_projects():
-    return jsonify({'projects' : projects})
+    projects = [project for project in db.sabroso.projects.find()]
+    return toJson(projects)
 
-@app.route('/api/v1.0/projects/<int:project_id>', methods=['GET'])
-def get_project(project_id):
-    project = [project for project in projects if project['_id'] == project_id]
-
-    if len(project) == 0 or not project:
+""" Get a single project by _id """
+@app.route('/api/v1.0/projects/<_id>', methods=['GET'])
+def get_project(_id):
+    if not _id:
         abort(404)
 
-    return jsonify({'project' : project[0]})
+    project = db.sabroso.projects.find_one({"_id" : ObjectId(_id)})
+    return toJson(project)
+
+""" Put a project """
+""" TODO: Use the ProjectAccessor for this task """
+@app.route('/api/v1.0/projects', methods=['POST'])
+def put_project():
+    if not 'name' in request.json:
+        abort(400)
+
+""" Make the 404 handler spit out JSON instead of html """
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(toJson({'error': 'Not found'}), 404)
+
+""" Handle 400 Bad Requests """
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(toJson({'error': 'Bad Request'}), 400)
+
+""" Convert data into json format for api presentation """
+def toJson(data):
+    return json.dumps(cursor_data, default=json_util.default)
 
 if __name__ == '__main__':
     app.run(debug=True)
