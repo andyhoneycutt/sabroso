db44526f927a1511a3c0126958963d393b72bfac
andyhoneycutt <ahoneycutt@gmail.com>
2015-07-13 20:17:51 -0600

    Settings rewrite, database abstraction
    More database abstraction, getting closer to better database backend
    flexibility

--- python/api/app.py
+++ python/api/app.py
@@ -1,33 +1,69 @@
 #!flask/bin/python
-from flask import Flask, abort, make_response
-from pymongo import MongoClient
-import json, bson
+"""
+app.py
+
+TODO: We need to abstract database handling and record
+stuff away from being mongo centric. Right now we have specific
+mongo errors being handled all over the place. Need to come up
+with a common data handler. Please, sweet baby jesus, do this
+soon.
+"""
+
+from flask import Flask, abort, make_response, g
 from bson import json_util, errors
 from bson.objectid import ObjectId
-from settings import db
+import pymongo
+import json
+import bson
+
+"""
+Configure the below for current api version as
+all requests must be directed to app_endpoint/<whatever>
+e.g. '/api/v1.0/projects/abcd1234', this handles the
+/api/v1.0 part of uri access
+"""
+base_endpoint = '/api'
+version = 'v1.0'
+url = '/'
+app_endpoint = url.join([base_endpoint, version])
 
 app = Flask(__name__)
+app.config.from_object('settings.DefaultSettings')
 
 """ Get a list of projects """
-@app.route('/api/v1.0/projects/', methods=['GET'])
+@app.route(app_endpoint + '/projects/', methods=['GET'])
 def get_projects():
-    projects = [project for project in db.projects.find()]
+    try:
+        projects = [project for project in get_database().projects.find()]
+    except (
+        pymongo.errors.ServerSelectionTimeoutError,
+        pymongo.errors.NetworkTimeout,
+        pymongo.errors.ConnectionFailure
+    ):
+        abort(500)
+    except pymongo.errors.AutoReconnect:
+        print("Had to reconnect, we can keep working")
     return toJson(projects)
 
 """ Get a single project by _id """
-@app.route('/api/v1.0/projects/<path:_id>', methods=['GET'])
+@app.route(app_endpoint + '/projects/<path:_id>/', methods=['GET'])
 def get_project(_id):
     if not _id:
         abort(404)
     try:
-        project = db.projects.find_one({"_id" : ObjectId(_id)})
+        project = get_database().projects.find_one({"_id" : ObjectId(_id)})
     except bson.errors.InvalidId:
         abort(400)
+    except pymongo.errors.ServerSelectionTimeoutError:
+        abort(500)
+    except pymongo.errors.AutoReconnect:
+        print("Had to reconnect, we can keep working")
+
     return toJson(project)
 
 """ Put a project """
 """ TODO: Use the ProjectAccessor for this task """
-@app.route('/api/v1.0/projects', methods=['POST'])
+@app.route(app_endpoint + '/projects/', methods=['POST'])
 def put_project():
     if not 'name' in request.json:
         abort(400)
@@ -42,9 +78,46 @@ def not_found(error):
 def bad_request(error):
     return make_response(toJson({'error': 'Bad Request'}), 400)
 
+""" Handle 500 Internal Server Error """
+@app.errorhandler(500)
+def internal_server_error(error):
+    return make_response(toJson({'error': 'Internal Server Error'}), 500)
+
+""" Close stuff out """
+@app.teardown_appcontext
+def do_teardown(error):
+    close_database_connections(error)
+
+""" Close Database Connections """
+def close_database_connections(error):
+    if hasattr(g, 'mongodb'):
+        try:
+            g.mongodb.client.close()
+        except TypeError as e:
+            print ("It looks like we lost mongodb before clean connection close, did it die?\n\t%s" % str(e))
+
 """ Convert data into json format for api presentation """
 def toJson(data):
     return json.dumps(data, default=json_util.default)
+
+""" Get a connection to our database """
+def get_database():
+    if not hasattr(g, 'mongodb'):
+        g.mongodb = connectToMongoDb()
+
+    return g.mongodb
+
+""" Connect to mongodb """
+def connectToMongoDb():
+    try:
+        mongo_connection = pymongo.MongoClient(app.config['MONGO_CONNECTION_URI'])
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to mongodb, is it running?\n\t%s" % str(e))
+        abort(500)
+
+    db = mongo_connection[app.config['MONGO_DATABASE']]
+
+    return db
 
 if __name__ == '__main__':
     app.run(debug=True)
--- python/api/settings.py
+++ python/api/settings.py
@@ -7,27 +7,14 @@ Configuration for the API package
 """
 __author__ = 'Andy Honeycutt'
 
-import pymongo
+class DefaultSettings(object):
+    MONGO_HOST = 'localhost'
+    MONGO_DATABASE = 'sabroso'
+    MONGO_CONNECTION_URI = 'mongodb://localhost'
+    TESTING = False
+    DEBUG = False
 
-mongo_settings = {
-  'host' : 'localhost',
-  'database' : 'sabroso',
-  'username' : '',
-  'password' : '',
-  'port' : ''
-}
-
-""" Try to get a connection to MongoDB, or fail out """
-try:
-    connection_string = 'mongodb://'
-    if mongo_settings['username'] != '' and mongo_settings['password'] != '':
-        connection_string = connection_string + mongo_settings['username'] + ':' + mongo_settings['password'] + '@'
-    connection_string = connection_string + mongo_settings['host']
-    if mongo_settings['port'] != '':
-        connection_string = connection_string + ':' + mongo_settings['port']
-    connection_string = connection_string + '/'
-    mongo_connection = pymongo.MongoClient(connection_string)
-    
-    db = mongo_connection[mongo_settings['database']]
-except pymongo.errors.ConnectionFailure:
-    exit("Could not connect to MongoDB is it running?")
+class TestingSettings(DefaultSettings):
+    MONGO_DATABASE = 'sabroso_testing'
+    TESTING = True
+    DEBUG = True
